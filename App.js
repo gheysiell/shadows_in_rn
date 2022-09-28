e4ee23465f39432a3ad4f2a46a107ae8a3b704ec
gheysiell <gheysiellfarukzinn@hotmail.com>
2022-09-28 12:09:34 -0400

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Shadow } from 'react-native-shadow-2'
+import { StyleSheet, View, Text } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.wrapperTitle}>
+                <Text style={styles.title}>REACT-NATIVE SHADOWS</Text>
+            </View>
+            <View style={styles.wrapperBody}>
+                {/*With library*/}
+                <View style={styles.wrapperContentShadows}>
+                    <Text style={styles.wrapperContentShadowsText}>Using react-native-shadow-2 lib</Text>
+                    <Shadow distance={5} startColor={'#90e778'} endColor={"#000"}>
+                        <View style={styles.view1}/>
+                    </Shadow>
+                </View>
+
+                {/*With react-native onyl*/}
+                <View style={styles.wrapperContentShadows}>
+                    <Text style={styles.wrapperContentShadowsText}>Using react-native pure</Text>
+                    <View style={styles.view2}/>
+                </View>                
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+        width: '100%',
+        backgroundColor: "#000"
+    },
+    wrapperTitle: {
+        height: '20%',
+        width: '100%',
+        padding: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        color: "#fff",
+        fontSize: 25,
+        fontWeight: '700',
+        backgroundColor: '#000',
+        textShadowOffset: {
+            width: 2,
+            height: 3
+        },
+        textShadowColor: "#ddd",
+        textShadowRadius: 1,
+    },
+    wrapperBody: {
+        padding: 10,
+        height: '80%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    view1: {
+        borderRadius: 40,
+        height: 80,
+        width: 80,
+        backgroundColor: "#34EC00",
+        marginBottom: 40,
+    },
+    view2: {
+        borderRadius: 40,
+        height: 80,
+        width: 80,
+        backgroundColor: "#34EC00",
+        shadowColor: "#34EC00",
+        elevation: 35
+    },
+    wrapperContentShadows: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    wrapperContentShadowsText: {
+        fontSize: 19,
+        color: "#fff",
+        marginBottom: 20
+    }
 });
---
+++ README.md
@@ -0,0 +1,5 @@
+# shadows_in_rn
+
+<div> <h2> App made in react native for shadow demonstration with library and pure </h2> </div>
+<div> <h5> ° For instaling dependences, npm install </h5> </div>
+<div> <h5> ° For init project go into the same folder, and run npm start </h5> </div>
